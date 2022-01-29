a72c3b62eedae24d94f3373e7aeae9fa5546ec5b
Yuji Nakamura <ny5117@icloud.com>
2022-01-29 20:32:55 +0900

    通信処理をaxiosに変更

--- src/context/driver/vocabularyDriver.ts
+++ src/context/driver/vocabularyDriver.ts
@@ -2,43 +2,36 @@ import { Vocabulary, UpdatedVocab } from "context/domain/vocabulary";
 import VocabularyDriver, {
   VocabularyJson,
 } from "context/interface/driver/vocabulatyDriver";
+import axios from "axios";
 export default class VocabularyDriverImpl implements VocabularyDriver {
   async findAll(): Promise<VocabularyJson[]> {
-    const res = await fetch(
+    const res = await axios.get(
       `${process.env.REACT_APP_API_ENDPOINT}/vocabularies`
     );
-    return await res.json();
+    return res.data;
   }
 
   async add(vocabulary: Vocabulary): Promise<void> {
-    await fetch(`${process.env.REACT_APP_API_ENDPOINT}vocabularies`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(vocabulary),
-    });
+    await axios.post(
+      `${process.env.REACT_APP_API_ENDPOINT}/vocabularies`,
+      vocabulary
+    );
   }
 
   async update(id: number, vocabulary: UpdatedVocab): Promise<void> {
-    await fetch(`${process.env.REACT_APP_API_ENDPOINT}/vocabularies/${id}`, {
-      method: "PUT",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
+    await axios.put(
+      `${process.env.REACT_APP_API_ENDPOINT}/vocabularies/${id}`,
+      {
         word: vocabulary.vocab,
         translatedWord: vocabulary.translatedVocab,
         image: "test.jpg",
-      }),
-    });
+      }
+    );
   }
 
   async delete(id: number): Promise<void> {
-    await fetch(`${process.env.REACT_APP_API_ENDPOINT}/vocabularies/${id}`, {
-      method: "DELETE",
-    });
+    await axios.delete(
+      `${process.env.REACT_APP_API_ENDPOINT}/vocabularies/${id}`
+    );
   }
 }
--- src/context/presenter/components/Vocabularies/EditVocabularyModal.tsx
+++ src/context/presenter/components/Vocabularies/EditVocabularyModal.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styled from "styled-components";
 
 import { useForm, Controller } from "react-hook-form";
